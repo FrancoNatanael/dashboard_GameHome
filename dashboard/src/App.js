d45d7a24f0898f060ac2e348d6561a98d70b5095
FrancoNatanael <tomainofranco5@gmail.com>
2022-03-25 20:20:23 -0300

    Franco - estructura React

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import SmallCard from './components/SmallCard'
+import LastProduct from './components/LastProduct'
+import Categories from './components/Categories'
+import LastUserCreated from './components/LastUserCreated'
+import ListProducts from './components/ListProducts'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Footer />
+      <SmallCard /> 
+      <LastProduct/>
+      <Categories/>
+      <LastUserCreated/>
+      <ListProducts/>
+    </React.Fragment>
   );
 }
 
