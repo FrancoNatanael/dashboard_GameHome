4b32bc40eb1751505a4edbbc43a8b84ce74daf60
Flor Farías <florencia.fariasmm@gmail.com>
2022-03-29 10:45:47 -0300

    Flor - categoris

---
+++ dashboard/src/components/CardCategory.js
@@ -0,0 +1,10 @@
+function CardCategory(props) {
+    return(
+        <div className="container-card-categories">
+                <div>{props.category}</div>
+                <div className="count">{props.count}</div>
+            </div>
+    )
+}
+
+export default CardCategory
--- dashboard/src/components/Categories.js
+++ dashboard/src/components/Categories.js
@@ -1,70 +1,35 @@
 import React from "react";
+import CardCategory from "./CardCategory";
+import { useEffect, useState } from 'react';
 
 function Categories() {
+  const [categories, setCategories] = useState([]);
+
+
+  useEffect(() => {
+    fetch('api/products')
+      .then(response => {
+       return response.json();
+      })
+      .then(data => {
+        setCategories(data.countByCategory)
+    })
+}, [categories]);
+
   return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            Aca van las categorias desde la base de datos
-          </h5>
+    <div>
+            <h3 className="titulos">Total de productos por categoría:</h3>
+            
+                {categories.length === 0 && <p>Cargando...</p>}
+                {
+                    categories.map((category, i) => {
+                         let key = Object.entries(category) 
+                        return <CardCategory category={key[0][0]} count={key[0][1]} />
+                    })
+                }
+
+           
         </div>
-        <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
 
